86cf781a6ff094db3d85cf115c30de412aff16c7
Gabriel Rivera Safadi <gabriel.rivera.safadi@gmail.com>
2017-07-07 15:31:57 -0400

    Remove warnings from viewer.

--- src/app/components/viewer/viewer.component.ts
+++ src/app/components/viewer/viewer.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {map} from 'rxjs/operator/map';
+import {debounceTime} from 'rxjs/operator/debounceTime';
+import {distinctUntilChanged} from 'rxjs/operator/distinctUntilChanged';
+
+const states = ['a', 'b'];
 
 @Component({
   selector: 'app-viewer',
@@ -11,5 +17,16 @@ export class ViewerComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  public model: any;
+  public model1: any;
+
+  search = (text$: Observable<string>) =>
+    map.call(distinctUntilChanged.call(debounceTime.call(text$, 200)),
+      term => term.length < 2 ? [] : states.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10));
+
+  search1 = (text$: Observable<string>) =>
+    map.call(distinctUntilChanged.call(debounceTime.call(text$, 200)),
+      term => term.length < 2 ? [] : states.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10));
 
 }
