e5ddb43416c140319870428181cca8b511661649
Joel Rojas Soliz <joel.rojas.soliz@gmail.com>
2017-07-21 20:53:28 -0400

    Uncomment bootstrap slide component

--- src/app/components/viewer/viewer.component.ts
+++ src/app/components/viewer/viewer.component.ts
@@ -4,7 +4,7 @@ import {map} from 'rxjs/operator/map';
 import {debounceTime} from 'rxjs/operator/debounceTime';
 import {distinctUntilChanged} from 'rxjs/operator/distinctUntilChanged';
 import {MapService} from '../../services/map.service';
-import {NgbSlideEvent} from '@ng-bootstrap/ng-bootstrap/carousel/carousel';
+// import {NgbSlideEvent} from '@ng-bootstrap/ng-bootstrap';
 import {ChartService} from '../../services/chart.service';
 import {Subscription} from 'rxjs/Subscription';
 
@@ -57,7 +57,7 @@ export class ViewerComponent implements OnInit, OnDestroy, AfterViewInit {
     map.call(distinctUntilChanged.call(debounceTime.call(text$, 200)),
       term => term.length < 2 ? [] : states.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10));
   // EVENTS
-  onSlideChangeEvent($event: NgbSlideEvent) {
+  onSlideChangeEvent($event) {
     let currentSlideId = $event.current;
     currentSlideId = currentSlideId.split('-')[0];
     const layerPaintProp = 'fill-color';
