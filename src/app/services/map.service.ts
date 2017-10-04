318c1d990622f118fcaf7a01e1a2c59910820a1e
Gabriel Rivera Safadi <gabriel.rivera.safadi@gmail.com>
2017-10-04 14:18:26 -0400

    Limit zoom.

--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -200,6 +200,7 @@ export class MapService {
       style: 'mapbox://styles/gfdrr-dashboard/cj6qvntcf3umq2snyd8se5xir',
       center: [10, 35],
       zoom: 0.55,
+      maxZoom: 2,
       maxBounds: [[-180, -80], [180, 80]]
     });
   }
