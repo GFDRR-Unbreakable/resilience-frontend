b908fd996acc0f383cddd4282e83caf4a1870135
Chris <chris@nautilytics.com>
2020-06-19 16:25:46 -0400

    Revert "don't show welcome pop-up on privacy-notice page"
    This reverts commit d4a1cf431f7eb4b8d9d7b06969fc60de6ea4f5ce.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent {
     router.events
     .filter(event => event instanceof NavigationEnd)
     .subscribe(({urlAfterRedirects}:NavigationEnd) => {
-      if (urlAfterRedirects !== '/' && urlAfterRedirects !== '/privacy-notice' && !this.modalOpened) {
+      if (urlAfterRedirects !== '/' && !this.modalOpened) {
         this.modalOpened = true;
         this.modalService.open(WelcomeModalComponent, {
           size: 'lg'
